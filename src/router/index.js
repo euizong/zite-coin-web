348f5e328ab8aa4b42e1cabd9c1454d75f8aef1c
euizong <euizong@gmail.com>
2018-05-29 21:32:02 +0900

    새 파일:       README.md 	새 파일:       config/default.js 	새 파일:       config/dev.js 	새 파일:       package.json 	새 파일:       server.js 	새 파일:       src/App.vue 	새 파일:       src/client-entry.js 	새 파일:       src/config.js 	새 파일:       src/favicon.png 	새 파일:       src/index.html 	새 파일:       src/registerGlobals.js 	새 파일:       src/router/_import_node.js 	새 파일:       src/router/_import_web.js 	새 파일:       src/router/index.js 	새 파일:       src/server-entry.js 	새 파일:       src/shared/ssrMixin.js 	새 파일:       src/store/index.js 	새 파일:       src/vendor.js 	새 파일:       src/views/Foo.vue 	새 파일:       src/views/Full.vue 	새 파일:       src/views/HTTP404.vue 	새 파일:       src/views/Min.vue 	새 파일:       src/views/ShowErrorPage.vue 	새 파일:       src/views/Test.vue 	새 파일:       src/views/Web.vue 	새 파일:       webpack.config.client.js 	새 파일:       webpack.config.server.js 	새 파일:       yarn.lock

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// can't put import() here, because node will complain "SyntaxError: Unexpected token import"
+const _import = require('./_import_' + TARGET)
+
+Vue.use(Router)
+
+const routes = [
+  { 
+    path: '/', 
+    component:  _import('Full'),
+    children: [
+      {
+        path: 'test',
+        component: _import('Test')
+      },
+      {
+        path: 'foo',
+        component: _import('Foo')
+      }
+    ]
+  },
+  { path: '/', component: _import('Full') },
+  { path: '/foo/:id', name: 'foo', component: _import('Foo') },
+  { path: '/show-error-page', component: _import('ShowErrorPage') }
+]
+
+if (TARGET === 'web') {
+  routes.push(
+    // catch-all route must be placed at the last
+    { path: '*', component: _import('HTTP404') }
+  )
+}
+
+export default new Router({
+  mode: 'history',
+  routes
+})
---
+++ src/views/Test.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="foo">
+    <p>Test!234</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      a: 0
+    }
+  },
+
+  metaInfo: {
+    title: 'Vue SSR Boilerplate',
+    meta: [
+      { vmid: 'description', name: 'description', content: 'Vue SSR Boilerplate' }
+    ]
+  },
+
+  prefetch() {
+    return Promise.resolve({
+      a: 123
+    })
+  },
+
+  // will be called on server side. check your console
+  created() {
+    console.log(this.a) //eslint-disable-line
+  },
+
+  // won't run on server side
+  beforeMount() {
+    console.log(this.a) //eslint-disable-line
+  }
+}
+</script>
